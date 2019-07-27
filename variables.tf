7ba0452c4a8ec716f600754e751e5fe03f447e15
Omer Barel <barelomer@gmail.com>
2019-07-27 22:25:02 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "resource_group_name" {
-  description = "(Required) The name of the resource group in which to create the storage account. Changing this forces a new resource to be created"
+  description = "(Required) The name of the resource group in which to create the objects. Changing this forces a new resource to be created"
 }
 
 variable "location" {
@@ -12,55 +12,25 @@ variable "tags" {
 }
 
 variable "storage_account_spec" {
-  description = "map of key-values for the storage account object"
+  description = "map of key-values for the storage account object. See main.tf for valid keys"
   type        = "map"
   default     = {}
 }
 
 variable "service_plan_spec" {
-  description = "map of key-values for the service plan object"
+  description = "map of key-values for the service plan object. See main.tf for valid keys"
   type        = "map"
   default     = {}
 }
 
 variable "function_app_spec" {
-  description = "map of key-values for the function app object"
+  description = "map of key-values for the function app object. See main.tf for valid keys"
   type        = "map"
   default     = {}
 }
 
 variable "application_insights_spec" {
-  description = "map of key-values for the application insights object"
+  description = "map of key-values for the application insights object. See main.tf for valid keys"
   type        = "map"
   default     = {}
-}
-
-
-/*variable "service_plan_kind" {
-  description = "The kind of the App Service Plan to create. Possible values are Windows (also available as App), Linux and FunctionApp (for a Consumption Plan). Defaults to Windows. Changing this forces a new resource to be created"
-}
-
-variable "service_plan_sku" {
-  description = "A sku block as documented in https://www.terraform.io/docs/providers/azurerm/r/app_service_plan.html#sku"
-  type = "map"
-}
-
-variable "function_app_name" {
-  description = "Specifies the name of the Function App. Changing this forces a new resource to be created"
-}
-
-variable "function_app_settings" {
-  description = "A key-value pair of App Settings."
-  type        = "map"
-  default = {}
-}
-
-variable "application_insights_type" {
-  description = " Specifies the type of Application Insights to create. Valid values are Java, iOS, MobileCenter, Other, Phone, Store, Web and Node.JS"
-}
-
-variable "enable_https" {
-  description = "enable https on the storage account"
-  default = false
-}*/
-
+}sa
